b91eb57907101227de62db6ac696e9f20bb6f072
imran5644 <imi56444@gmail.com>
2020-11-12 21:33:26 +0530

    implement my_each with index enumerables

--- main.rb
+++ main.rb
@@ -1,12 +1,22 @@
 module Enumerable
-    def my_each
-        return to_enum(:my_each) unless block_given?
-        counter = 0
-        while counter < to_a.length
-            yield to_a[count]
-            counter += 1
-        end
-        self
+    
+def my_each
+    return to_enum(:my_each) unless block_given?
+    counter = 0
+    while counter < to_a.length
+        yield to_a[count]
+        counter += 1
     end
+    self
+end
 
     
+def my_each_with_index
+    return to enum(:my_each_with_index) unless block_given?
+    counter = 0
+    while counter < to_a.length
+        yield (to_a[counter], counter)
+        counter += 1
+    end 
+    self
+end
